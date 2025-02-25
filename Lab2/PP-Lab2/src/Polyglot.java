0d2662f5a84e5c79451165ac1138bf6daf1ac22c
Tzigu <andrei.tigaieriu@student.tuiasi.ro>
2025-02-25 15:51:14 +0200

    Task 2 done

--- Lab2/PP-Lab2/src/Polyglot.java
+++ Lab2/PP-Lab2/src/Polyglot.java
@@ -1,6 +1,8 @@
 //import libraria principala polyglot din graalvm
 import org.graalvm.polyglot.*;
 
+import java.util.Arrays;
+
 //clasa principala - aplicatie JAVA
 class Polyglot {
     //metoda privata pentru conversie low-case -> up-case folosind functia toupper() din R
@@ -20,11 +22,25 @@ class Polyglot {
 
     //metoda privata pentru evaluarea unei sume de control simple a literelor unui text ASCII, folosind PYTHON
     private static int SumCRC(String token){
+        //Aici elimin primul si ultimul caracter din sir
+        token = removeFirstAndLastChar(token);
+
         //construim un context care ne permite sa folosim elemente din PYTHON
         Context polyglot = Context.newBuilder().allowAllAccess(true).build();
         //folosim o variabila generica care va captura rezultatul excutiei functiei PYTHON, sum()
         //avem voie sa inlocuim anumite elemente din scriptul pe care il construim spre evaluare, aici token provine din JAVA, dar va fi interpretat de PYTHON
-        Value result = polyglot.eval("python", "sum(ord(ch) for ch in '" + token + "')");
+
+        int[] coefficients = {1 ,2 ,3 ,4 ,5};
+        String coeffs = Arrays.stream(coefficients).mapToObj(String::valueOf).collect(java.util.stream.Collectors.joining(", "));
+        String multiLineString = "coeffs = [%s]\n" +
+                "total = 0\n" +
+                "for i, ch in enumerate('%s', 1):\n" +
+                "    power = min(i, 5)\n" +
+                "    total += coeffs[power] * ord(ch)\n" +
+                "total";
+        String pythonExpression = String.format(multiLineString, coeffs, token);
+
+        Value result = polyglot.eval("python", pythonExpression);
         //utilizam metoda asInt() din variabila incarcata cu output-ul executiei, pentru a mapa valoarea generica la un Int
         int resultInt = result.asInt();
         // inchidem contextul Polyglot
@@ -33,12 +49,20 @@ class Polyglot {
         return resultInt;
     }
 
+    public static String removeFirstAndLastChar(String inputString) {
+        if (inputString == null || inputString.length() < 2) {
+            throw new IllegalArgumentException("Șirul trebuie să aibă cel puțin două caractere.");
+        }
+        return inputString.substring(1, inputString.length() - 1);
+    }
+
+
     //functia MAIN
     public static void main(String[] args) {
         //construim un context pentru evaluare elemente JS
         Context polyglot = Context.create();
         //construim un array de string-uri, folosind cuvinte din pagina web:  https://chrisseaton.com/truffleruby/tenthings/
-        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\",\"command\",\"included\",\"in\",\"GraalVM\",\"we\",\"will\",\"be\",\"automatically\",\"using\",\"the\",\"Graal\",\"JIT\",\"compiler\",\"no\",\"extra\",\"configuration\",\"is\",\"needed\",\"I\",\"will\",\"use\",\"the\",\"time\",\"command\",\"to\",\"get\",\"the\",\"real\",\"wall\",\"clock\",\"elapsed\",\"time\",\"it\",\"takes\",\"to\",\"run\",\"the\",\"entire\",\"program\",\"from\",\"start\",\"to\",\"finish\",\"rather\",\"than\",\"setting\",\"up\",\"a\",\"complicated\",\"micro\",\"benchmark\",\"and\",\"I\",\"will\",\"use\",\"a\",\"large\",\"input\",\"so\",\"that\",\"we\",\"arent\",\"quibbling\",\"about\",\"a\",\"few\",\"seconds\",\"here\",\"or\",\"there\",\"The\",\"large.txt\",\"file\",\"is\",\"150\",\"MB\"];");
+        Value array = polyglot.eval("js", "[\"If\",\"we\",\"run\",\"the\",\"java\"];");//,\"command\",\"included\",\"in\",\"GraalVM\",\"we\",\"will\",\"be\",\"automatically\",\"using\",\"the\",\"Graal\",\"JIT\",\"compiler\",\"no\",\"extra\",\"configuration\",\"is\",\"needed\",\"I\",\"will\",\"use\",\"the\",\"time\",\"command\",\"to\",\"get\",\"the\",\"real\",\"wall\",\"clock\",\"elapsed\",\"time\",\"it\",\"takes\",\"to\",\"run\",\"the\",\"entire\",\"program\",\"from\",\"start\",\"to\",\"finish\",\"rather\",\"than\",\"setting\",\"up\",\"a\",\"complicated\",\"micro\",\"benchmark\",\"and\",\"I\",\"will\",\"use\",\"a\",\"large\",\"input\",\"so\",\"that\",\"we\",\"arent\",\"quibbling\",\"about\",\"a\",\"few\",\"seconds\",\"here\",\"or\",\"there\",\"The\",\"large.txt\",\"file\",\"is\",\"150\",\"MB\"];");
         //pentru fiecare cuvant, convertim la upcase folosind R si calculam suma de control folosind PYTHON
         for (int i = 0; i < array.getArraySize();i++){
             String element = array.getArrayElement(i).asString();
