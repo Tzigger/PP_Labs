d7c5e8b0fd21eee31f3b7bb8a10e65eee1bc6623
Tzigu <andrei.tigaieriu@student.tuiasi.ro>
2025-03-11 15:01:09 +0200

    Lab4 example test

---
+++ Lab3/PP-Lab3/untitled/src/main/kotlin/org/example/Content.kt
@@ -0,0 +1,13 @@
+package org.example
+
+class Content{
+    private val author : String = "author" ;
+    private val text : String = "text";
+    private val  name : String = "name";
+    private val publisher : String = "publisher";
+
+    public fun getAuthor(){
+        
+    }
+
+}
